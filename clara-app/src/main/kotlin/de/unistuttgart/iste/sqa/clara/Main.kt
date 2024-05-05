020713f14866c6033c18620ebec5b20e51bd5415
Elias Müller <elias_benedikt.mueller@porsche.de>
2024-05-05 15:21:39 +0200

    build: try other approach

--- clara-app/src/main/kotlin/de/unistuttgart/iste/sqa/clara/Main.kt
+++ clara-app/src/main/kotlin/de/unistuttgart/iste/sqa/clara/Main.kt
@@ -6,9 +6,9 @@ import java.util.concurrent.TimeUnit
 fun main() {
     val config = ClaraConfig.loadFrom("/app/resources/config.yml")
     // Workaround to get the kube-config in the container at runtime
-    Runtime.getRuntime().exec("mkdir /root/.kube").waitFor(1, TimeUnit.SECONDS)
-    Runtime.getRuntime().exec("cp /app/resources/config /root/.kube/config").waitFor(1, TimeUnit.SECONDS)
-
+    // Runtime.getRuntime().exec("mkdir /root/.kube").waitFor(1, TimeUnit.SECONDS)
+    // Runtime.getRuntime().exec("cp /app/resources/config /root/.kube/config").waitFor(1, TimeUnit.SECONDS)
+    System.setProperty("kubeconfig", "/app/resources/config")
 
     val app = App(config)
 
